adab56999f9e3acb3060b45fd330bc543e4f61a2
Abdullah <113207637+Pokerfacez1@users.noreply.github.com>
2022-10-08 19:52:40 +0300

    first commit

---
+++ LAP1op2.c
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+using namespace std;
+
+void trig_func()
+{
+	double x;
+	cin>>x;
+	cout<<"Sin value of x = "<<sin(x)<<endl;
+	cout<<"Cos value of x = "<<cos(x)<<endl;
+	          
+}
+
+int main()
+{
+	trig_func();
+
+	return 0 ;
+}
